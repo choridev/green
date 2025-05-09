6ea64dee512ca6d58284505970db48ac32c7bd7f
Chori <yidodeveloper@gmail.com>
2025-05-09 17:02:16 +0900

    Initial Commit

---
+++ green/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  green
+//
+//  Created by Huicheol Yi on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
